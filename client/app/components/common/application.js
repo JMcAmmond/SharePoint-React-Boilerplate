82f1bee4c8fd0da640f0cb4085b95c57ffb06250
Jeff McAmmond <jeffmcammond@gmail.com>
2018-08-19 18:16:33 -0600

    Removing import

--- client/app/components/common/application.js
+++ client/app/components/common/application.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { render } from 'react-dom';
 import { HashRouter, Route, Link } from "react-router-dom";
 import { Home, Example, About } from '../../components';
 
